64ef2e42cb8084d5d77fc23fb9baa4ea4d5d0206
arturosanchez <mobileproyectsarturosm97.asm@gmail.com>
2025-09-24 14:15:08 -0600

    Refactor(domain): Refactor domain errors

--- domain/src/main/java/com/asm/domain/errors/GeneralFailure.kt
+++ domain/src/main/java/com/asm/domain/errors/GeneralFailure.kt
@@ -9,26 +9,7 @@ sealed class GeneralError {
     data class ClientError(val code: String): GeneralError()
 }
 
-sealed class GamerError {
-    data object GamerNotExists: GamerError()
-    data class General(val generalError: GeneralError): GamerError()
-}
-
-sealed class SessionError {
-    data object SessionNotExists: SessionError()
-    data class General(val generalError: GeneralError): SessionError()
-}
-
-enum class GamerError2 {
-
-}
-
-fun GeneralError.toGamerError() = GamerError.General(this)
-fun GeneralError.toSessionError() = SessionError.General(this)
 fun GeneralError.toUnsuccessful() = Result.Unsuccessful(this)
-
-fun GamerError.toUnsuccessful() = Result.Unsuccessful(this)
-fun SessionError.toUnsuccessful() = Result.Unsuccessful(this)
 
 
 sealed class TimerFailure {
