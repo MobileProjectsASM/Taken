45d7b90d01fea00d697e4e28d2ee12fc07ee8b0e
arturosanchez <mobileproyectsarturosm97.asm@gmail.com>
2026-01-05 12:00:36 -0600

    Feat(Domain): Add create account use case

---
+++ domain/src/main/java/com/asm/domain/use_cases/CreateAccountUC.kt
@@ -0,0 +1,33 @@
+package com.asm.domain.use_cases
+
+import com.asm.domain.entities.Result
+import com.asm.domain.errors.GeneralError
+import com.asm.domain.errors.toUnsuccessful
+import com.asm.domain.repositories.AuthRepository
+import com.asm.domain.use_cases.base.UseCaseSync
+import com.asm.domain.utils.Logger
+import javax.inject.Inject
+
+class CreateAccountUC @Inject constructor(
+    private val logger: Logger,
+    private val authRepository: AuthRepository
+): UseCaseSync<Result<String, GeneralError>, CreateAccountUC.CreateAccountParams>() {
+
+    companion object {
+        const val TAG = "create_account_use_case"
+    }
+
+    data class CreateAccountParams(
+        val email: String,
+        val password: String
+    )
+
+    override suspend fun run(params: CreateAccountParams): Result<String, GeneralError> {
+        return try {
+            authRepository.createAccount(params.email, params.password)
+        } catch (exception: Exception) {
+            logger.logE(TAG, exception)
+            GeneralError.Unknown.toUnsuccessful()
+        }
+    }
+}
