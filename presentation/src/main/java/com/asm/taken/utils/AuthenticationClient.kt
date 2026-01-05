18b2257012aac582d0dde80d5308a544db67115f
arturosanchez <mobileproyectsarturosm97.asm@gmail.com>
2026-01-05 13:21:55 -0600

    Refactor(AuthenticationClient): Clean AuthenticationClient

--- presentation/src/main/java/com/asm/taken/utils/AuthenticationClient.kt
+++ presentation/src/main/java/com/asm/taken/utils/AuthenticationClient.kt
@@ -1,39 +1,22 @@
 package com.asm.taken.utils
 
 import android.app.Activity
-import android.content.Context
 import android.util.Log
-import androidx.activity.result.ActivityResultRegistryOwner
 import androidx.credentials.ClearCredentialStateRequest
 import androidx.credentials.CredentialManager
-import androidx.credentials.CustomCredential
-import androidx.credentials.GetCredentialRequest
-import androidx.credentials.GetCredentialResponse
-import androidx.credentials.exceptions.GetCredentialException
 import com.asm.domain.entities.AuthUser
 import com.asm.domain.entities.Result
 import com.asm.domain.errors.GeneralError
 import com.asm.domain.errors.toUnsuccessful
-import com.asm.taken.R
 import com.facebook.AccessToken
-import com.facebook.CallbackManager
-import com.facebook.FacebookCallback
-import com.facebook.FacebookException
-import com.facebook.login.LoginManager
-import com.facebook.login.LoginResult
-import com.google.android.libraries.identity.googleid.GetGoogleIdOption
-import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
 import com.google.firebase.FirebaseException
 import com.google.firebase.FirebaseNetworkException
 import com.google.firebase.FirebaseTooManyRequestsException
 import com.google.firebase.auth.AuthCredential
-import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseAuthException
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
 import com.google.firebase.auth.FirebaseAuthInvalidUserException
 import com.google.firebase.auth.FirebaseAuthMissingActivityForRecaptchaException
-import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.PhoneAuthCredential
 import com.google.firebase.auth.PhoneAuthOptions
 import com.google.firebase.auth.PhoneAuthProvider
@@ -41,8 +24,6 @@ import com.google.firebase.auth.PhoneAuthProvider.OnVerificationStateChangedCall
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
-import java.security.MessageDigest
-import java.util.UUID
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
@@ -55,86 +36,6 @@ class AuthenticationClient @Inject constructor(
         const val TAG: String = "AuthenticationClient"
     }
 
-    suspend fun createAccount(email: String, password: String): Result<Unit, GeneralError> {
-        return try {
-            val createAccountResult = auth.createUserWithEmailAndPassword(email, password).await()
-            val firebaseUser = createAccountResult.user
-            if (firebaseUser == null) {
-                Log.e(TAG, "FirebaseUser is null")
-                return GeneralError.ServerError().toUnsuccessful()
-            }
-            Result.Successful(Unit)
-        } catch (exception: Exception) {
-            Log.e(TAG, "Unexpected exception to create account", exception)
-            when (exception) {
-                is FirebaseAuthException -> GeneralError.ClientError("")
-                is FirebaseNetworkException -> GeneralError.NetworkError
-                is FirebaseException -> GeneralError.ClientError("")
-                else -> GeneralError.Unknown
-            }.toUnsuccessful()
-        }
-    }
-
-    suspend fun signInWithEmailAndPassword(
-        email: String,
-        password: String
-    ): Result<AuthUser, GeneralError> {
-        return try {
-            val authResult = auth.signInWithEmailAndPassword(email, password).await()
-            val firebaseUser = authResult.user
-            if (firebaseUser == null) {
-                Log.e(TAG, "FirebaseUser is null")
-                return GeneralError.ServerError().toUnsuccessful()
-            }
-            Result.Successful(AuthUser(firebaseUser.uid, firebaseUser.photoUrl?.toString()))
-        } catch (exception: Exception) {
-            Log.e(TAG, "Unexpected Exception to sign in with email and password", exception)
-            when (exception) {
-                is FirebaseAuthInvalidUserException -> GeneralError.ClientError("")
-                is FirebaseAuthInvalidCredentialsException -> GeneralError.ClientError("")
-                is FirebaseNetworkException -> GeneralError.NetworkError
-                is FirebaseException -> GeneralError.ClientError("")
-                else -> GeneralError.Unknown
-            }.toUnsuccessful()
-        }
-    }
-
-    fun signInWithFacebook(
-        activityResultRegistryOwner: ActivityResultRegistryOwner,
-        coroutineScope: CoroutineScope,
-        onFacebookLoginLoading: () -> Unit,
-        onAuthResult: (Result<AuthUser, GeneralError>) -> Unit
-    ) {
-        val callbackManager = CallbackManager.Factory.create()
-        LoginManager.getInstance()
-            .registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
-                override fun onCancel() {
-                    Log.e(TAG, "Unexpected Error cancel process")
-                    onAuthResult(GeneralError.ClientError().toUnsuccessful())
-                }
-
-                override fun onError(error: FacebookException) {
-                    Log.e(TAG, "Unexpected Exception to sign in with facebook", error)
-                    onAuthResult(GeneralError.ServerError().toUnsuccessful())
-                }
-
-                override fun onSuccess(result: LoginResult) {
-                    coroutineScope.launch {
-                        onFacebookLoginLoading()
-                        val accessToken = result.accessToken.token
-                        val credential = FacebookAuthProvider.getCredential(accessToken)
-                        val authResult = signInWithFirebase(credential)
-                        onAuthResult(authResult)
-                    }
-                }
-            })
-        LoginManager.getInstance().logInWithReadPermissions(
-            activityResultRegistryOwner,
-            callbackManager,
-            listOf("email", "public_profile")
-        )
-    }
-
     fun getCurrentUserSocialNetworkImage(): Result<String?, GeneralError> = try {
         val socialNetworkImage = auth.currentUser?.photoUrl?.toString()?.let { baseUrl ->
             AccessToken.getCurrentAccessToken()?.token?.let { "$baseUrl?access_token=$it" }
@@ -143,9 +44,6 @@ class AuthenticationClient @Inject constructor(
     } catch (exception: Exception) {
         Result.Unsuccessful(GeneralError.Unknown)
     }
-
-    suspend fun signInWithGoogle(context: Context): Result<AuthUser, GeneralError> =
-        signInWithCredentialManager(context, true)
 
     fun authWithPhoneNumber(
         activity: Activity,
@@ -201,30 +99,6 @@ class AuthenticationClient @Inject constructor(
         return signInWithFirebase(phoneAuthCredential)
     }
 
-    private suspend fun signInWithCredentialManager(
-        context: Context,
-        authorizedAccounts: Boolean
-    ): Result<AuthUser, GeneralError> {
-        return try {
-            val credentialRequest = buildCredentialRequest(authorizedAccounts)
-            val credentialResponse = credentialManager.getCredential(context, credentialRequest)
-            try {
-                val authCredential = handleCredentialResponse(credentialResponse)
-                signInWithFirebase(authCredential)
-            } catch (exception: Exception) {
-                Log.e(TAG, "Unexpected exception to handle credentials", exception)
-                GeneralError.ClientError().toUnsuccessful()
-            }
-        } catch (exception: GetCredentialException) {
-            Log.e(TAG, "Unexpected exception to get credentials", exception)
-            if (authorizedAccounts) signInWithCredentialManager(context, false)
-            else GeneralError.ClientError().toUnsuccessful()
-        } catch (exception: Exception) {
-            Log.e(TAG, "Unexpected exception to get credentials", exception)
-            GeneralError.Unknown.toUnsuccessful()
-        }
-    }
-
     private suspend fun signInWithFirebase(authCredential: AuthCredential): Result<AuthUser, GeneralError> {
         return try {
             val authResult = auth.signInWithCredential(authCredential).await()
@@ -260,23 +134,6 @@ class AuthenticationClient @Inject constructor(
         }
     }
 
-    private fun buildCredentialRequest(authorizedAccounts: Boolean): GetCredentialRequest {
-        val rawNonce = UUID.randomUUID().toString()
-        val bytes = rawNonce.toByteArray()
-        val md = MessageDigest.getInstance("SHA-256")
-        val digest = md.digest(bytes)
-        val hashedNonce = digest.fold("") { str, it -> "$str${"%02x".format(it)}" }
-        val googleIdOption = GetGoogleIdOption.Builder()
-            .setFilterByAuthorizedAccounts(authorizedAccounts)
-            .setServerClientId(resourceResolver.getString(R.string.default_web_client_id))
-            .setAutoSelectEnabled(true)
-            .setNonce(hashedNonce)
-            .build()
-        return GetCredentialRequest.Builder()
-            .addCredentialOption(googleIdOption)
-            .build()
-    }
-
     private fun getPhoneAuthOptions(
         activity: Activity,
         phoneNumber: String,
@@ -288,24 +145,5 @@ class AuthenticationClient @Inject constructor(
             .setActivity(activity)
             .setCallbacks(onVerificationStateChangedCallbacks)
             .build()
-    }
-
-    private fun handleCredentialResponse(credentialResponse: GetCredentialResponse): AuthCredential {
-        when (val credential = credentialResponse.credential) {
-            is CustomCredential -> {
-                val credentialType = credential.type
-                if (credentialType == GoogleIdTokenCredential.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL) {
-                    val googleIdTokenCredential =
-                        GoogleIdTokenCredential.createFrom(credential.data)
-                    return GoogleAuthProvider.getCredential(googleIdTokenCredential.idToken, null)
-                } else {
-                    throw Exception("Unexpected type of credential")
-                }
-            }
-
-            else -> {
-                throw Exception("Unexpected type of credential")
-            }
-        }
     }
 }
