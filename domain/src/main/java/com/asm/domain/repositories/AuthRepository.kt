73072a75c250f58dd33d08e2d7f0a2ac5bd86171
arturosanchez <mobileproyectsarturosm97.asm@gmail.com>
2026-08-17 19:16:48 -0600

    Refactor(AuthRepository): Change named method

--- domain/src/main/java/com/asm/domain/repositories/AuthRepository.kt
+++ domain/src/main/java/com/asm/domain/repositories/AuthRepository.kt
@@ -7,7 +7,8 @@ import com.asm.domain.errors.GeneralError
 
 interface AuthRepository {
     suspend fun authWithEmailAndPassword(email: String, password: String): Result<AuthUser, GeneralError>
-    suspend fun authWithToken(token: String, providerId: ProviderId): Result<AuthUser, GeneralError>
+    suspend fun authWithOAuth(token: String, providerId: String): Result<AuthUser, GeneralError>
+    suspend fun authWithOpenIdConnect(token: String, providerId: String): Result<AuthUser, GeneralError>
     suspend fun authWithOTP(sessionId: String, otp: String): Result<AuthUser, GeneralError>
     suspend fun createAccount(email: String, password: String): Result<Unit, GeneralError>
     suspend fun getAuthUser(): Result<AuthUser, GeneralError>
