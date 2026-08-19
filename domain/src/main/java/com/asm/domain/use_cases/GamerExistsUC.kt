7480604e1625d9cbac9e7adee85b2806f6d7a166
arturosanchez <mobileproyectsarturosm97.asm@gmail.com>
2026-08-19 14:10:52 -0600

    Refactor(Domain): Refactor login domain

--- domain/src/main/java/com/asm/domain/use_cases/GamerExistsUC.kt
+++ domain/src/main/java/com/asm/domain/use_cases/GamerExistsUC.kt
@@ -1,8 +1,7 @@
 package com.asm.domain.use_cases
 
 import com.asm.domain.entities.Result
-import com.asm.domain.errors.GeneralError
-import com.asm.domain.errors.toUnsuccessful
+import com.asm.domain.errors.Failure
 import com.asm.domain.repositories.GamerRepository
 import com.asm.domain.use_cases.base.UseCaseSync
 import com.asm.domain.utils.Logger
@@ -11,18 +10,18 @@ import javax.inject.Inject
 class GamerExistsUC @Inject constructor(
     private val gamerRepository: GamerRepository,
     private val logger: Logger
-): UseCaseSync<Result<Boolean, GeneralError>, String>() {
+): UseCaseSync<Result<Boolean, Failure>, String>() {
 
     companion object {
         const val TAG = "GamerExistsUC"
     }
 
-    override suspend fun run(params: String): Result<Boolean, GeneralError> {
+    override suspend fun run(params: String): Result<Boolean, Failure> {
         return try {
             gamerRepository.verifyGamerExists(params)
         } catch (exception: Exception) {
             logger.logE(TAG, exception)
-            GeneralError.Unknown.toUnsuccessful()
+            Result.Unsuccessful(Failure.UnexpectedFailure)
         }
     }
 }
