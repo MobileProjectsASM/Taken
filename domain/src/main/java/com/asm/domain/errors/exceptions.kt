cefdc95504efec2ce24f392c2c2c2c7daede149c
arturosanchez <mobileproyectsarturosm97.asm@gmail.com>
2026-08-23 23:08:28 -0600

    Refactor(domain): Refactor domain source login

--- domain/src/main/java/com/asm/domain/errors/exceptions.kt
+++ domain/src/main/java/com/asm/domain/errors/exceptions.kt
@@ -1,3 +1,4 @@
 package com.asm.domain.errors
 
 class AuthProcessException(message: String, cause: Throwable? = null): Exception(message, cause)
+class ApiServiceException(message: String, cause: Throwable? = null): Exception(message, cause)
--- domain/src/main/java/com/asm/domain/repositories/CountryInfoRepository.kt
+++ domain/src/main/java/com/asm/domain/repositories/CountryInfoRepository.kt
@@ -2,9 +2,9 @@ package com.asm.domain.repositories
 
 import com.asm.domain.entities.CountryInfo
 import com.asm.domain.entities.Result
-import com.asm.domain.errors.Failure
+import com.asm.domain.errors.CommonFailure
 
 interface CountryInfoRepository {
-    suspend fun getCountriesInfoSortedByName(ascending: Boolean = true): Result<List<CountryInfo>, Failure>
-    suspend fun downloadCountriesInfo(): Result<Unit, Failure>
+    suspend fun getCountriesInfoSortedByName(ascending: Boolean = true): Result<List<CountryInfo>, CommonFailure>
+    suspend fun downloadCountriesInfo(): Result<Unit, CommonFailure>
 }
